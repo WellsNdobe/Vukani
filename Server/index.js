ca4e3f83583e9c1b59e61beadf5a24cef2ac78f3
Daniel Ndobe <danielndobe850@gmail.com>
2025-09-09 12:14:25 +0200

    Initiated Backend

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,28 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
 const app = express();
-const PORT = 3000;
 
-// Middleware to parse JSON
-app.use(express.json());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
-// Simple route
+
+const CONNECTION_URL = "mongodb+srv://Wells:dK7Q1ZpeCCzGQsvI@cluster0.br62ojx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello to Vukani API');
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+
