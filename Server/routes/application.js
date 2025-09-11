aac65f10b3a2f659740fbf56c6f731eaa9d9c10d
Daniel Ndobe <danielndobe850@gmail.com>
2025-09-11 13:41:24 +0200

    Protected Application Routes

--- Server/routes/application.js
+++ Server/routes/application.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { createApplication, getApplicationsByJob, getApplicationsByUser } from "../controllers/application.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Apply for a job
-router.post("/", createApplication);
+router.post("/", auth, createApplication);
 
 // Get all applications for a job (company use-case)
-router.get("/job/:jobId", getApplicationsByJob);
+router.get("/job/:jobId", auth, getApplicationsByJob);
 
 // Get all applications by a user (applicant use-case)
-router.get("/user/:userId", getApplicationsByUser);
+router.get("/user/:userId", auth, getApplicationsByUser);
 
 export default router;
--- Server/routes/jobs.js
+++ Server/routes/jobs.js
@@ -1,15 +1,13 @@
 
 import express from 'express';
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 import { createJob, getJobs, getJobById } from '../controllers/jobs.js';
 
 router.get('/', getJobs);
-router.post('/createJob', createJob);
+router.post('/createJob', auth, createJob);
 router.get("/:id", getJobById);
-
-
-
 
 export default router;
